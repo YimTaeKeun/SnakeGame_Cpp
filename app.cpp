b322782beb522a91a020bf1edc89363e3fc1ea01
YimTaekeun <ytk030305@gmail.com>
2024-01-24 22:42:51 +0900

    ✨: Success Initiallize Logic-#5

--- app.cpp
+++ app.cpp
@@ -1,18 +1,28 @@
-#include "snake.h"
+#include "snake.cpp"
 #include <iostream>
 using namespace std;
 bool*** board;
 int board_size;
 void printBoard();
-void initiallizeBoard(int);
+void initiallizeBoard();
 int main(){
     cout << "Input Board Size: ";
     cin >> board_size;
-    initiallizeBoard(board_size);
+    Snake mySnake(board, board_size);
+    printBoard();
     return 0;
 }
-void initiallizeBoard(int board_size){
-    
+void initiallizeBoard(){
+    board = new bool**[board_size];
+    for(int i = 0; i < board_size; i++){
+        board[i] = new bool*[board_size];
+        for(int j= 0; j < board_size; j++){
+             board[i][j] = new bool[2];
+             board[i][j][0] = false;
+             board[i][j][1] = false;
+        }
+    }
+    printBoard();
 }
 void printBoard(){
     for(int i = 0; i < board_size + 2; i++) cout << "- ";
@@ -20,8 +30,8 @@ void printBoard(){
     for(int i = 0; i < board_size; i++){
         cout << "- ";
         for(int j = 0; j < board_size; j++){
-            if(board[i][j][0]) cout << "■ ";
-            else if(board[i][j][1]) cout << "● ";
+            if(board[i][j][0]) cout << "B ";
+            else if(board[i][j][1]) cout << "A ";
             else cout << "* ";
         }
         cout << "-" << endl;
--- snake.cpp
+++ snake.cpp
@@ -1,21 +1,25 @@
 #include "snake.h"
-Snake::Snake(bool*** board, int size):board_size(size){
+Snake::Snake(bool***& board, int size):board_size(size){
     mode = directions[0];
     board = new bool**[size];
     for(int i = 0; i < size; i++){
         board[i] = new bool*[size];
-        for(int j = 0; j < size; j++) board[i][j] = new bool[2];
+        for(int j = 0; j < size; j++){
+             board[i][j] = new bool[2];
+             board[i][j][0] = false;
+             board[i][j][1] = false;
+        }
     }
     board[0][0][0] = true;
-    dq.push_back({0, 0});
+    dq.push_back(new int[2] {0, 0});
 }
-bool Snake::moveBody(bool*** board){
+bool Snake::moveBody(bool***& board){
     //board -> {{{False, False}, 열}, 행}
     int goCol = mode[0], goRow = mode[1];
     int* cal = dq.front();
     int movedCol = cal[0] + goCol, movedRow = cal[1] + goRow;
     if(movedCol < 0 || movedRow < 0 || movedCol == board_size || movedRow == board_size) return false;
-    dq.push_front({movedCol, movedRow});
+    dq.push_front(new int[2] {movedCol, movedRow});
     board[movedCol][movedRow][0] = true;
     if(!board[movedCol][movedRow][1]){
         int* tail = dq.back();
